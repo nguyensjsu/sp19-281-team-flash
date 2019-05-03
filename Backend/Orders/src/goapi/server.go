8f52c83fd13d523dae311db1cf6824209c442120
Mili Patel <mili.patel@sjsu.edu>
2019-05-02 23:43:56 -0700

    ping api for orders

---
+++ Backend/Orders/src/goapi/server.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/rs/cors"
+	// "github.com/streadway/amqp"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/satori/go.uuid"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+) 
+// MongoDB Config 
+
+var mongodb_server = "192.168.99.100:27017"
+var mongodb_database = "records" 
+var mongodb_collection_orders = "orders"
+var mongodb_collection_userid = "userid"
+ 
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	corsObj := cors.New(cors.Options{
+        AllowedOrigins: []string{"*"},
+        AllowedMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+        AllowedHeaders: []string{"Accept", "content-type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+	})
+	
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.Use(corsObj)
+	n.UseHandler(mx)
+	return n
+}
+
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	
+}
+
+
+
+// Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
+
